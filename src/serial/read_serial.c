b826e6508e0df71a9493a8d195111565c43434ae
felipegutierrez <felipe.gutierrez@tu-berlin.de>
2018-08-29 15:08:21 +0200

    writing and reading from the serial port UART.

--- src/serial/read_serial.c
+++ src/serial/read_serial.c
@@ -17,40 +17,65 @@
 #include "read_serial.h"
 
 int read_serial() {
+	struct termios options;
+
 	/**
 	 * The argument flags must include one of the following access modes:
 	 * O_RDONLY, O_WRONLY, or O_RDWR. These request opening the file
 	 * read-only, write-only, or read/write, respectively.
 	 */
-	int sfd = open("/dev/serial0", O_RDONLY | O_NOCTTY);
+	int sfd = open("/dev/serial0", O_RDWR | O_NOCTTY);
 	if (sfd == -1) {
 		printf("Error no is : %d\n", errno);
 		printf("Error description is : %s\n", strerror(errno));
+		return EXIT_FAILURE;
 	} else {
 		printf("Successfully opened port /dev/serial0\n");
 	}
 
-	struct termios options;
 	tcgetattr(sfd, &options);
 
-	cfsetspeed(&options, B9600);
+	cfsetspeed(&options, B9600); // set the baud rate to 9600
 	cfmakeraw(&options);
 
+	// Set up Serial Configuration
 	options.c_cflag &= ~CSTOPB;
 	options.c_cflag |= CLOCAL;
 	options.c_cflag |= CREAD;
 	options.c_cc[VTIME] = 1;
 	options.c_cc[VMIN] = 100;
+
+	// Apply configuration
 	tcsetattr(sfd, TCSANOW, &options);
 
+	// Attempt to write data
+	char buf[] = "hello world";
+	printf("Sending: %s\n", buf);
+	int wcount = write(sfd, buf, strlen(buf));
+	usleep(100000);
+
+	if (wcount < 0) {
+		perror("Write");
+		return EXIT_FAILURE;
+	} else {
+		printf("Sent %d characters\n", wcount);
+	}
+
+	// Attempt to read data
 	char buf2[100];
 	int bytes;
 	ioctl(sfd, FIONREAD, &bytes);
-	if (bytes != 0) {
-		int count = read(sfd, buf2, 100);
+	// if (bytes != 0) {
+	int rcount = read(sfd, buf2, sizeof(buf2));
+	if (rcount < 0) {
+		perror("Read");
+		return EXIT_FAILURE;
+	} else {
+		printf("Received %d characters\n", rcount);
 	}
-
-	printf("%s\n\r", buf2);
+	// buf2[count] = '\0';
+	printf("Received: %s\n\r", buf2);
+	// }
 
 	close(sfd);
 	printf("Successfully closed port /dev/serial0\n");
